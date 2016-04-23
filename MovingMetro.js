c2601dc4972a1dac2617b14e8462b4e272783987
Trollhag <oscar.trollhag@gmail.com>
2016-04-23 15:54:37 +0200

    v1.1

--- MovingMetro.js
+++ MovingMetro.js
@@ -1,18 +1,10 @@
-$(document).ready(function() {
-    $('.moving-tiles .tile').each(function() {
-        if ($(this).children('.tile-slide').length > 0) {
-            $(this).data('MovingTiles', new TileSlide({ target: this }))
-        }
-    })
-});
-
 function TileSlide(args) {
     if (!args) args = {};
     this.wait = parseInt(args.Wait) || 3000;
-    this.Speed = parseInt(args.Speed) || 800;
+    this.speed = parseInt(args.Speed) || 800;
     this.direction = (args.Direction || "right").toLowerCase();
     this.pause = false;
-    this.slides = $(args.target).find('.tile-wrapper');
+    this.slides = $(args.Target).find('.tile-wrapper');
     this.animation = {
         "right": {
             "left": "-=" + this.slides.first().width() + "px",
@@ -33,10 +25,10 @@ function TileSlide(args) {
     };
     this.block = false;
     var that = this;
-    $(args.target).mouseenter(function() {
+    $(args.Target).mouseenter(function() {
         that.Pause() 
     });
-    $(args.target).mouseleave(function()  {
+    $(args.Target).mouseleave(function()  {
         that.Resume()
     });
     
@@ -47,22 +39,6 @@ function TileSlide(args) {
 }
 TileSlide.prototype = {
     Next: function() {
-        this.Animate(this.direction);
-    },
-    Prev: function() {
-        var direction = "left"
-        if      (this.direction == "down")  direction = "up";
-        else if (this.direction == "up")    direction = "down";
-        else if (this.direction == "left")  direction = "right";
-        
-        this.Animate(direction);
-    },
-    Animate: function(direction) {
-        var that = this;
-        
-        if (this.block) return;
-        this.block = true;
-        var block = 0;
         
         var active;
         if (this.slides.filter('.active').length > 0)               active = this.slides.filter('.active');
@@ -70,16 +46,48 @@ TileSlide.prototype = {
         else                                                        active = this.slides.filter(':first-child');
         
         var next;
-        if (this.slides.filter('[data-slide="' + parseInt(active.attr('[data-slide="')) + 1 + '"]').length > 0) next = this.slides.filter('[data-slide="' + parseInt(active.attr('[data-slide="')) + 1 + '"]');
-        else if (active.next().length > 0)                                                                      next = active.next();
-        else                                                                                                    next = this.slides.filter(':first-child');
+        if (this.slides.filter('[data-slide="' + parseInt(active.attr('[data-slide="')) + 1 + '"]').length > 0) {
+            next = this.slides.filter('[data-slide="' + parseInt(active.attr('[data-slide="')) + 1 + '"]');
+        }
+        else if (active.next().length > 0)  next = active.next();
+        else                                next = this.slides.filter(':first-child');
+        
+        this.Animate(active, next, this.direction);
+    },
+    Prev: function() {
+        var direction = "left"
+        if      (this.direction == "down")  direction = "up";
+        else if (this.direction == "up")    direction = "down";
+        else if (this.direction == "left")  direction = "right";
+        
+        var active;
+        if (this.slides.filter('.active').length > 0)               active = this.slides.filter('.active');
+        else if (this.slides.filter('[data-slide="1"]').length > 0) active = this.slides.filter('[data-slide="1"]');
+        else                                                        active = this.slides.filter(':first-child');
+        
+        var prev;
+        if (this.slides.filter('[data-slide="' + parseInt(active.attr('[data-slide="')) - 1 + '"]').length > 0) {
+            prev = this.slides.filter('[data-slide="' + parseInt(active.attr('[data-slide="')) - 1 + '"]');
+        }
+        else if (active.prev().length > 0)  prev = active.next();
+        else                                prev = this.slides.filter(':last-child');
+        
+        this.Animate(active, prev, direction);
+    },
+    Animate: function(active, next, direction) {
+        var that = this;
+        
+        if (this.block) return;
+        this.block = true;
+        var block = 0;
         
         this.Load(next, next.attr('data-direction') || direction);
+        this.slides.first().closest('.tile-slide').addClass('M2-Animating');
         active.animate(this.animation[next.attr('data-direction') || direction], {
             duration:   this.speed, 
             queue:      false,
             complete:   function() {
-                active.removeClass('active').css({ top: '', bottom: '', left: '', right: '' });
+                active.removeClass('active').css({ top: '', bottom: '', left: '', right: '', "z-index": "" });
                 checkDone();
             }
         });
@@ -96,6 +104,7 @@ TileSlide.prototype = {
             block++
             if (block == 2) {
                 that.block = false;
+                that.slides.first().closest('.tile-slide').removeClass('M2-Animating');
             }
         }
     },
@@ -116,19 +125,23 @@ TileSlide.prototype = {
         var complete = {
             "right": {
                 "left": this.slides.first().width() + "px",
-                "top": "0"
+                "top": "0",
+                "z-index": 10
             },
             "left": {
                 "left": "-" + this.slides.first().width() + "px",
-                "top": "0"
+                "top": "0",
+                "z-index": 10
             },
             "up": {
                 "top": "-" + this.slides.first().height() + "px",
-                "left": "0"
+                "left": "0",
+                "z-index": 10
             },
             "down": {
                 "top": this.slides.first().height() + "px",
-                "left": "0"
+                "left": "0",
+                "z-index": 10
             }
         }
         elm.css(complete[direction]);
